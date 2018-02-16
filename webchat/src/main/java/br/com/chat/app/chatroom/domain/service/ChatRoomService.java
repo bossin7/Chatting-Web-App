2a96f07dbbbab4ed220e30477c601adb310c003b
bossin7 <bapesapplik@yahoo.com>
2018-02-16 05:40:22 +0100

    Update ChatRoomService.java

--- webchat/src/main/java/br/com/chat/app/chatroom/domain/service/ChatRoomService.java
+++ webchat/src/main/java/br/com/chat/app/chatroom/domain/service/ChatRoomService.java
@@ -2,9 +2,9 @@ package br.com.chat.app.chatroom.domain.service;
 
 import java.util.List;
 
-import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.ChatRoom;
-import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.ChatRoomUser;
-import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.InstantMessage;
+import br.com.chat.app.chatroom.domain.model.ChatRoom;
+import br.com.chat.app.chatroom.domain.model.ChatRoomUser;
+import br.com.chat.app.chatroom.domain.model.InstantMessage;
 
 public interface ChatRoomService {
 	
