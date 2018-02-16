b3bdb169a190f08fde3dcbeab18174008f538874
bossin7 <bapesapplik@yahoo.com>
2018-02-16 06:13:26 +0100

    Update IntegrationTestsSuite.java

--- webchat/src/test/java/br/com/chat/app/integrationTests/IntegrationTestsSuite.java
+++ webchat/src/test/java/br/com/chat/app/integrationTests/IntegrationTestsSuite.java
@@ -3,12 +3,12 @@ package br.com.chat.app.integrationTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import br.com.jorgeacetozi.ebookChat.integrationTests.authentication.api.AuthenticationControllerTest;
-import br.com.jorgeacetozi.ebookChat.integrationTests.authentication.domain.service.DefaultUserServiceTest;
-import br.com.jorgeacetozi.ebookChat.integrationTests.chatroom.api.ChatRoomControllerTest;
-import br.com.jorgeacetozi.ebookChat.integrationTests.chatroom.domain.service.CassandraInstantMessageServiceTest;
-import br.com.jorgeacetozi.ebookChat.integrationTests.chatroom.domain.service.RedisChatRoomServiceTest;
-import br.com.jorgeacetozi.ebookChat.integrationTests.test.AbstractIntegrationTest;
+import br.com.chat.app.integrationTests.authentication.api.AuthenticationControllerTest;
+import br.com.chat.app.integrationTests.authentication.domain.service.DefaultUserServiceTest;
+import br.com.chat.app.integrationTests.chatroom.api.ChatRoomControllerTest;
+import br.com.chat.app.integrationTests.chatroom.domain.service.CassandraInstantMessageServiceTest;
+import br.com.chat.app.integrationTests.chatroom.domain.service.RedisChatRoomServiceTest;
+import br.com.chat.app.integrationTests.test.AbstractIntegrationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
