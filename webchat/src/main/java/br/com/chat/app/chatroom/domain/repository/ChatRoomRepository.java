1248248a1402e9cbb733f7fafd258822d2a51887
bossin7 <bapesapplik@yahoo.com>
2018-02-16 05:37:40 +0100

    Update ChatRoomRepository.java

--- webchat/src/main/java/br/com/chat/app/chatroom/domain/repository/ChatRoomRepository.java
+++ webchat/src/main/java/br/com/chat/app/chatroom/domain/repository/ChatRoomRepository.java
@@ -2,7 +2,7 @@ package br.com.chat.app.chatroom.domain.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.ChatRoom;
+import br.com.chat.app.chatroom.domain.model.ChatRoom;
 
 public interface ChatRoomRepository extends CrudRepository<ChatRoom, String> {
 
