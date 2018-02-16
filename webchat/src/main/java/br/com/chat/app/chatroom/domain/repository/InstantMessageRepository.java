5cc05fbec2841d19556f01ed286bdba59edce6bd
bossin7 <bapesapplik@yahoo.com>
2018-02-16 05:38:14 +0100

    Update InstantMessageRepository.java

--- webchat/src/main/java/br/com/chat/app/chatroom/domain/repository/InstantMessageRepository.java
+++ webchat/src/main/java/br/com/chat/app/chatroom/domain/repository/InstantMessageRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-import br.com.jorgeacetozi.ebookChat.chatroom.domain.model.InstantMessage;
+import br.com.chat.app.chatroom.domain.model.InstantMessage;
 
 public interface InstantMessageRepository extends CassandraRepository<InstantMessage> {
 	
