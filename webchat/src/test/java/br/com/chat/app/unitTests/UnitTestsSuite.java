00e9929e29ea5e216453aab1d4d1122261997cd2
bossin7 <bapesapplik@yahoo.com>
2018-02-16 06:20:09 +0100

    Update UnitTestsSuite.java

--- webchat/src/test/java/br/com/chat/app/unitTests/UnitTestsSuite.java
+++ webchat/src/test/java/br/com/chat/app/unitTests/UnitTestsSuite.java
@@ -3,10 +3,10 @@ package br.com.chat.app.unitTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import br.com.jorgeacetozi.ebookChat.unitTests.chatroom.domain.service.RedisChatRoomServiceTest;
-import br.com.jorgeacetozi.ebookChat.unitTests.chatroom.domain.model.InstantMessageBuilderTest;
-import br.com.jorgeacetozi.ebookChat.unitTests.utils.DestinationsTest;
-import br.com.jorgeacetozi.ebookChat.unitTests.utils.SystemMessagesTest;
+import br.com.chat.app.unitTests.chatroom.domain.service.RedisChatRoomServiceTest;
+import br.com.chat.app.unitTests.chatroom.domain.model.InstantMessageBuilderTest;
+import br.com.chat.app.unitTests.utils.DestinationsTest;
+import br.com.chat.app.unitTests.utils.SystemMessagesTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
