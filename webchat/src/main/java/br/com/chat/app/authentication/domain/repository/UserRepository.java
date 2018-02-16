3b75fec3ce39f6cfcaef4be351cc3c12fe6a8798
bossin7 <bapesapplik@yahoo.com>
2018-02-16 05:22:19 +0100

    Update UserRepository.java

--- webchat/src/main/java/br/com/chat/app/authentication/domain/repository/UserRepository.java
+++ webchat/src/main/java/br/com/chat/app/authentication/domain/repository/UserRepository.java
@@ -2,7 +2,7 @@ package br.com.chat.app.authentication.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.jorgeacetozi.ebookChat.authentication.domain.model.User;
+import br.com.chat.app.authentication.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
